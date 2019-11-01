9ac2a0f2e0487138832826c65484d3943a656150
xmy180719 <15281835886@163.com>
2019-11-01 16:55:45 +0800

    2.O分支增加“SO EASY”

--- src/com/gitproject/TestUser.java
+++ src/com/gitproject/TestUser.java
@@ -2,7 +2,7 @@ package com.gitproject;
 
 public class TestUser {
     public static void main(String[] args) {
-        System.out.println("这是1.0分支新增的输出");
+        System.out.println("这是1.0分支新增的输出,SO EASY");
         User user = new User();
         user.setAddress("广东佛山");
         user.setAge(16);
