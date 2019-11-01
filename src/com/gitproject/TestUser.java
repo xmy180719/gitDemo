784cad6e9ecc4ae6d47f4aecb1515178c719f2ed
xmy180719 <15281835886@163.com>
2019-11-01 17:07:29 +0800

    master分支增加代码

--- src/com/gitproject/TestUser.java
+++ src/com/gitproject/TestUser.java
@@ -3,6 +3,7 @@ package com.gitproject;
 public class TestUser {
     public static void main(String[] args) {
         System.out.println("这是1.0分支新增的输出");
+        System.out.println("测试冲突！");
         User user = new User();
         user.setAddress("广东佛山");
         user.setAge(16);
