53466db8ce9d0b95d4de7a5855848878a050e35c
xmy180719 <15281835886@163.com>
2019-11-01 15:51:44 +0800

    主干分支新增Teacher类

---
+++ src/com/gitproject/Teacher.java
@@ -0,0 +1,40 @@
+package com.gitproject;
+
+public class Teacher {
+    private String name;
+    private String address;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/com/gitproject/test.java
+++
@@ -1,7 +0,0 @@
-package com.gitproject;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("this is my first gitproject!");
-    }
-}
